1fea40c29ba62d77aeeea604aabb4de3a5efa1bd
lms-30 <lahinirikomarasylvain30@gmail.com>
2026-03-11 17:56:27 +0300

    fix: remove unnecessary public modifiers in test class (SonarQube)

--- src/test/java/com/example/ApplicationTest.java
+++ src/test/java/com/example/ApplicationTest.java
@@ -1,12 +1,14 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ApplicationTest {
+class ApplicationTest {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
         assertTrue(true);
     }
+
 }
